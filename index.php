0a1d6ed73a87900ead1507e6acaf383ff6a93ec4
maricamola <maricamola98@libero.it>
2023-05-17 16:32:21 +0200

    Milestone 1 completed

--- index.php
+++ index.php
@@ -18,17 +18,18 @@
 $error_msg = '';
 $password = '';
 
-if (isset($_GET['length']) && $_GET['length'] > 8 && $_GET['length'] <= 15) {
+if (isset($_GET['length']) && is_numeric($_GET['length']) && $_GET['length'] >= 8 && $_GET['length'] <= 15) {
   $length = $_GET['length'];
   $password = generatePassword($length);
   $password_msg = 'La password generata è:';
-} else {
+} else if (isset($_GET['length'])) {
   $error_msg = 'Inserire un numero compreso tra 8-15';
 }
 
 function generatePassword($length)
 {
   $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+  //substr serve ad estrarre un parte della stringa restituita da str_shuffl
   //str_shuffle mescola tutti i caratteri in una stringa
   $password = substr(str_shuffle($chars), 0, $length);
   return $password;
@@ -52,7 +53,7 @@ require_once __DIR__ . '/partials/head.php';
           aria-label="default input example">
         <p class="text-danger"><?php echo $error_msg ?></p>
         <button type="submit" class="btn btn-primary my-3">Genera Password</button>
-        <p class="text">La password è : <?php echo $password ?></p>
+        <p class="text"><?php echo $password_msg ?? '' ?> <?php echo $password ?></p>
     </form>
   </div>
 
